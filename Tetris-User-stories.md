2ba24ba02b4d6a9ee016ff6a617992226c2e9862
Onida <OnidaSurshana@gmail.com>
2019-07-29 23:05:14 -0400

    working on the javascript

--- Tetris-User-stories.md
+++ Tetris-User-stories.md
@@ -1,21 +1,28 @@
 As a developer i want to link the game to the home page so that the user can browse the game selections
 
-As a developer i want to make a 10x20 gridded box for the gaming board. 
+As a User i want a 10x20 gridded box for the gaming board.  (maybe use a constructor function here too)
 
-As a developer i should make atleast 8 different block shapes
+As a developer i want to write a constructor function to make blocks for me.
+
+As a user i want atleast 3 [8 ] different block shapes
 
 As a developer i should ensure that the blocks stack and dont overlap with each other 
 
 As a developer i should write some code that clears out a row of blocks when filled 
 
-As a developer i should make an imput box for the user to write their name
+As a i want an imput box for to write my name
 
 As a user i'd like to see my current score and level
+
+As a user i want to be awarded more points for clearing multiple rows at the same time4
 
 (low Prior) As a user id like to see the highest scores of all time on the home page [All the highest scores from all games]
 
 (Low prior) As a user i'd like to see the next upcoming block [3 upcoming blocks] in a preview box
 
+[As a user i want there to be 6 difficulty settings (starting at 0) takeing me up 5 levels for each one]
+
 [AS a user i want to hear the classic tetris music]
 
+[[[As a user i want to be able to play with a friend]]] 
 
--- js/tetris.js
+++ js/tetris.js
@@ -1,16 +1,190 @@
+//Create arrays within arrays for the I/Line shape and every possible rotation (must be 4x4)
+
+//Create arrays within arrays for the J shape and every possible rotation (must be 3x3)
+// const J = [
+// 	[
+// 		[1, 0, 0],
+// 		[1, 1, 1],
+// 		[0, 0, 0]
+// 	],
+// 	[
+// 		[0, 1, 1],
+// 		[0, 1, 0],
+// 		[0, 1, 0]
+// 	],
+// 	[
+// 		[0, 0, 0],
+// 		[1, 1, 1],
+// 		[0, 0, 1]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[0, 1, 0],
+// 		[1, 1, 0]
+// 	]
+// ];
+//Create arrays within arrays for the L shape and every possible rotation (must be 3x3)
+// const L = [
+// 	[
+// 		[0, 0, 1],
+// 		[1, 1, 1],
+// 		[0, 0, 0]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[0, 1, 0],
+// 		[0, 1, 1]
+// 	],
+// 	[
+// 		[0, 0, 0],
+// 		[1, 1, 1],
+// 		[1, 0, 0]
+// 	],
+// 	[
+// 		[1, 1, 0],
+// 		[0, 1, 0],
+// 		[0, 1, 0]
+// 	]
+// ];
+//Create an array for the O/box shape (must be 4x4)
+// const O = [
+// 	[
+// 		[0, 0, 0, 0],
+// 		[0, 1, 1, 0],
+// 		[0, 1, 1, 0],
+// 		[0, 0, 0, 0],
+// 	]
+// ];
+//Create arrays within arrays for the S shape and every possible rotation (must be 3x3)
+// const S = [
+// 	[
+// 		[0, 1, 1],
+// 		[1, 1, 0],
+// 		[0, 0, 0]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[0, 1, 1],
+// 		[0, 0, 1]
+// 	],
+// 	[
+// 		[0, 0, 0],
+// 		[0, 1, 1],
+// 		[1, 1, 0]
+// 	],
+// 	[
+// 		[1, 0, 0],
+// 		[1, 1, 0],
+// 		[0, 1, 0]
+// 	]
+// ];
+//Create arrays within arrays for the T shape and every possible rotation (must be 3x3)
+// const T = [
+// 	[
+// 		[0, 1, 0],
+// 		[1, 1, 1],
+// 		[0, 0, 0]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[0, 1, 1],
+// 		[0, 1, 0]
+// 	],
+// 	[
+// 		[0, 0, 0],
+// 		[1, 1, 1],
+// 		[0, 1, 0]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[1, 1, 0],
+// 		[0, 1, 0]
+// 	]
+// ];
+//Create arrays within arrays for the Z shape and every possible rotation (must be 3x3)
+// const Z = [
+// 	[
+// 		[1, 1, 0],
+// 		[0, 1, 1],
+// 		[0, 0, 0]
+// 	],
+// 	[
+// 		[0, 0, 1],
+// 		[0, 1, 1],
+// 		[0, 1, 0]
+// 	],
+// 	[
+// 		[0, 0, 0],
+// 		[1, 1, 0],
+// 		[0, 1, 1]
+// 	],
+// 	[
+// 		[0, 1, 0],
+// 		[1, 1, 0],
+// 		[1, 0, 0]
+// 	]
+// ];
+
 //get the canvas from html
+const cvs = document.getElementById("tetris")
+//context of the canvas
+const ctx = cvs.getContext("2d")
 
-//each square/canvas grid should be 20x20 px
+//create a variable for the row and collum 
+const ROW = 20;
+const COL = COLUMN = 10;
+const SQ = squareSize = 20;
 
+//since theyre are going to be squares that wont actually be counted set them to a invisible color
+const VACANT = "WHITE";
+
+//next you should actually create the shaped, use https://www.w3schools.com/tags/canvas_fillrect.asp this method
+// ctx.fillStyle = "red";
+// ctx.fillRect(0,0,50,50);
+
+//next do the strokes
+// ctx.strokeStyle = "black";
+// ctx.strokeRect(0,0,50,50);
+
+//you might want to put that code into a function, i assume you'll need it often
+function drawSquare(x,y,color) {
+  // use color instead of typing out every color
+  ctx.fillStyle = color;
+  //use x & y variables for the locations instead of numbers
+  ctx.fillRect(x*SQ,y*SQ,SQ,SQ);
+
+  //make a square variable and use it in place of the size numbers
+  ctx.strokeStyle = "BLACK";
+  ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
+}
+
+//Dario sudgested making the canvas itself an empty array
+
+let board = [];
+//create the rows with a loop
+for ( r = 0; r < ROW; r++){
+board[r] = [];
+//create the colors with a loop
+for (c = 0; c < COL; c ++){
+//white for now
+board[r][c] = VACANT;
+  }
+}
+
+//then draw the board on the canvas
+function drawBoard(){
+  for( r = 0; r < ROW; r++){
+    for(c = 0; c < COL; C++){
+      drawSquare(c,r,board[r][c]);
+    }
+  }
+}
+
+drawBoard();
 //get the context of canvas to get methods and properties and allow use in JS
 
-//draw rectangle 
+//Create arrays to make the variable positions for each shape (might want to do this in another JS file)
 
-//set positions 
-
-//set sizes 
-
-//set strokes 
 
 //since each square on each shape is 20 px as well as the grid we can set a var/const for sqaures instead of counting 20px
 
@@ -23,8 +197,6 @@
 //to control the shapes and rotations you should create 3x3 squares using arrays and set the ones not being used to empty while the squares with actual squares in it should be set to 1, the rotation axis should obviously be in the center
 
 //like dario sudgested make an array inside an array for every shape 
-
-
 
 
 
