09da534e69a453ffe018e56b76fff57d158d29ee
Maria-Elena Farmer <mfarmer_89@hotmail.com>
2019-07-30 11:51:53 -0400

    checks vertical/ horizontal wins

--- js/connect-four.js
+++ js/connect-four.js
@@ -14,21 +14,22 @@ var gridPosition = [
 ];
 
 //locate empty row to fill
-function findRow(selectedColumn) {
+function findRow(selectedColumn) { 
   var i = 0;
-  // console.log(gridPosition[i][selectedColumn]);
   while(gridPosition[i][selectedColumn] === 0) {
-    // console.log(gridPosition[i][selectedColumn]);
     i++;
   }
+  //add player # to array
   if (player === "player1") {
     gridPosition[i-1][selectedColumn] = 1;
   } else {
     gridPosition[i-1][selectedColumn] = 2;
   }
+  //save selected row number
   selectedRow = i-1;
 }
 
+//find cell location in single array of cells
 function getSelectedCell(selectedRow, selectedColumn) {
   selectedCell = selectedColumn + (selectedRow * 7);
   return selectedCell;
@@ -42,6 +43,49 @@ function switchPlayer() {
   }
 }
 
+function checkHorizontal(playerNumber) {
+  var rowCount = 0;
+  for(var i = 0; i < gridPosition.length; i++) {
+    for(var j = 0; j < gridPosition[i].length; j++) {
+      if(gridPosition[i][j] === playerNumber) {
+        rowCount++;
+        if(rowCount === 4) {
+          alert(`Player${playerNumber} wins!`);
+          break;
+        }
+      } else {
+        rowCount = 0;
+      }
+    }
+    rowCount = 0;
+  }
+}
+
+function checkVertical(playerNumber) {
+  var colCount = 0;
+  for(var j = 0; j < gridPosition[0].length; j++) {
+    for(var i = 0; i < gridPosition.length; i++) {
+      if(gridPosition[i][j] === playerNumber) {
+        colCount++;
+        if(colCount === 4) {
+          alert(`Player${playerNumber} wins!`);
+          break;
+        }
+      } else {
+        colCount = 0;
+      }
+    }
+    colCount = 0;
+  }
+}
+
+function checkForWin () {
+  checkHorizontal(1);
+  checkHorizontal(2);
+  checkVertical(1);
+  checkVertical(2);
+}
+
 //event handler
 var grid = document.getElementById("grid");
 grid.addEventListener("click", function(event) {
@@ -51,5 +95,6 @@ grid.addEventListener("click", function(event) {
   getSelectedCell(selectedRow, selectedColumn);
   var allCells = document.querySelectorAll(".cell");
   allCells[selectedCell].setAttribute("class", "cell " + player);
+  checkForWin();
   switchPlayer();
 });
